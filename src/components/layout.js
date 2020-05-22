a02c905c482ba6ff81dcb4f18411a8df6102d088
mattbudde <matt@designbymatt.org>
2020-05-22 07:32:31 +0100

    Init

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,9 @@
-import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-import Navigation from "../components/navigation"
+import React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Navigation from '../components/navigation';
 import 'prismjs/themes/prism-okaidia.css';
+import { Gradient } from 'react-gradient';
+import '../styles/layout.css';
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -14,19 +16,39 @@ export default ({ children }) => {
         }
       }
     `
-  )
+  );
   return (
-    <div className="site-wrapper">
-      <header className="site-header">
-        <div className="site-title">
-          <Link to="/">{data.site.siteMetadata.title}</Link>
+    <React.Fragment>
+      <Gradient
+        gradients={[
+          ['rgb(231, 75, 255)', 'rgb(255, 243, 75)'],
+          ['rgb(255, 78, 167)', 'rgb(167, 255, 78)'],
+          ['rgb(255, 127, 63)', 'rgb(63, 197, 255)'],
+          ['rgb(255, 228, 74)', 'rgb(195, 74, 255)'],
+        ]}
+        property="border"
+        element="div"
+        className="gradient"
+      >
+        <div className="site-wrapper">
+          <header className="site-header">
+            <div className="site-title">
+              <Link to="/">{data.site.siteMetadata.title}</Link>
+            </div>
+            <Navigation />
+          </header>
+          {children}
+          <footer className="site-footer">
+            <p>
+              &copy; {new Date().getFullYear()} Matt Budde &bull; Crafted with{' '}
+              <span role="img" aria-label="love">
+                ❤️
+              </span>{' '}
+              and <a href="https://gatsbyjs.org">Gatsby</a>
+            </p>
+          </footer>
         </div>
-        <Navigation />
-      </header>
-      {children}
-      <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
-      </footer>
-    </div>
-  )
-}
+      </Gradient>
+    </React.Fragment>
+  );
+};
--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,21 +1,27 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
+import { motion } from 'framer-motion';
 
 const PostLink = ({ post }) => (
-  <article className="card ">
-    <Link to={post.frontmatter.path}>
-      {!!post.frontmatter.thumbnail && (
-        <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title + "- Featured Shot"} />
-      )}
-    </Link>
-    <header>
-      <h2 className="post-title">
-        <Link to={post.frontmatter.path} className="post-link">
-          {post.frontmatter.title}
-        </Link>
-      </h2>
-      <div className="post-meta">{post.frontmatter.date}</div>
-    </header>
-  </article>
-)
-export default PostLink
+  <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+    <article className="card ">
+      <Link to={post.frontmatter.path}>
+        {!!post.frontmatter.thumbnail && (
+          <img
+            src={post.frontmatter.thumbnail}
+            alt={post.frontmatter.title + '- Featured Shot'}
+          />
+        )}
+      </Link>
+      <header>
+        <h2 className="post-title">
+          <Link to={post.frontmatter.path} className="post-link">
+            {post.frontmatter.title}
+          </Link>
+        </h2>
+        <div className="post-meta">{post.frontmatter.date}</div>
+      </header>
+    </article>
+  </motion.div>
+);
+export default PostLink;
